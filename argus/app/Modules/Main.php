d01920d75c5b6afd6941beeb050773f8b0ce1b0d
Mr Naeem <mrnaeem@tutanota.com>
2025-08-14 10:16:23 +0700

    fix: add error handling in guzzle

--- argus/app/Modules/Main.php
+++ argus/app/Modules/Main.php
@@ -91,7 +91,7 @@ class Main
                                 $results = $scoring->run();
                             } catch (\GuzzleHttp\Exception\RequestException $e) {
                                 
-                                if(!empty($results['opencti']['errors'])) {
+                                if(!$results['status']) {
                                     if($threatIntelResult['status']) {
                                         $scoring2 = new \App\Modules\HashScoring($threatIntelResult['data']);
                                         $results = $scoring2->run();
